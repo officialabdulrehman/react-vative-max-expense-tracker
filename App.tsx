6991bbe4bd43e4587bf6726ee10a7cc9f7c7c3c4
NizTheDev <arehman08@hotmail.com>
2022-12-07 17:43:05 +0500

    feat: added basic navigation setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { styles } from "./App.styles";
+import { ExpenseManage } from "./src/screens/ExpenseManage/ExpenseManage.screen";
+import { ExpensesAll } from "./src/screens/ExpensesAll/ExpensesAll.screen";
+import { ExpensesRecent } from "./src/screens/ExpensesRecent/ExpensesRecent.screen";
+import { Screens } from "./src/screens/Screens.enum";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const NavigationBottomTabs = () => {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen
+        name={Screens.ExpensesRecent}
+        component={ExpensesRecent}
+      />
+      <BottomTabs.Screen name={Screens.ExpensesAll} component={ExpensesAll} />
+    </BottomTabs.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Expense Tracker</Text>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name={Screens.ExpenseManage}
+            component={ExpenseManage}
+          />
+          <Stack.Screen
+            name={Screens.NavigationBottomTabs}
+            component={NavigationBottomTabs}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
---
+++ src/screens/Screens.enum.ts
@@ -0,0 +1,6 @@
+export enum Screens {
+  ExpenseManage = "ExpenseManage",
+  ExpensesRecent = "ExpensesRecent",
+  ExpensesAll = "ExpensesAll",
+  NavigationBottomTabs = "NavigationBottomTabs"
+}
