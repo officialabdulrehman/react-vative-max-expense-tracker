006d10f0598ac279530cf9b44c890b247c8e08d9
NizTheDev <arehman08@hotmail.com>
2022-12-09 23:56:54 +0500

    feat: styled the custom Input component

--- src/components/UI/Input/Input.styles.ts
+++ src/components/UI/Input/Input.styles.ts
@@ -2,12 +2,25 @@ import { DynamicStyleSheet } from "../../../styles/Dynamic.StyleSheet";
 
 export const styles = DynamicStyleSheet.create((props) => ({
   container: {
-
+    marginHorizontal: 5,
+    marginVertical: 10
   },
-  text: {
-
+  label: {
+    fontSize: 14,
+    color: props.colors.primary400,
+    marginBottom: 4,
+    marginLeft: 4
   },
   textInput: {
-
-  }
+    color: props.textColors.primary400,
+    backgroundColor: props.backgroundColors.primary100,
+    borderRadius: 8,
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    fontSize: 16
+  },
+  textInputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top"
+  },
 }))
--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -8,10 +8,15 @@ type Props = {
 
 export const Input = (props: Props) => {
   const { label, config } = props;
+
+  const inputStyles: Partial<TextInputProps>["style"][] = [styles.textInput];
+  if (config?.multiline) {
+    inputStyles.push(styles.textInputMultiline);
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{label}</Text>
-      <TextInput style={styles.textInput} {...config} />
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...config} />
     </View>
   );
 };
